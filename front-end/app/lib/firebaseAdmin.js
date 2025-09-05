5976c8a8bcef1fb80a742092c6f27e46203d0dfa
Ducanh-techno <leanh0961@gmail.com>
2025-09-05 16:49:26 +0700

    Replace repo content 12

--- front-end/app/lib/firebaseAdmin.js
+++ front-end/app/lib/firebaseAdmin.js
@@ -1,12 +1,6 @@
 import admin from "firebase-admin";
-import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS, "utf8")
-);
-
-// Nếu vẫn lỗi, thử xử lý private_key
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
