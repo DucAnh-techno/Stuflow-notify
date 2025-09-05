307c7ea072bb08ead5610653f6d84d5320c3205f
Ducanh-techno <leanh0961@gmail.com>
2025-09-05 16:55:09 +0700

    Replace repo content 13

--- back-end/lib/firebaseAdmin.js
+++ back-end/lib/firebaseAdmin.js
@@ -1,10 +1,16 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
 if (!admin.apps.length) {
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined;
+
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey,
+    }),
   });
 }
 
